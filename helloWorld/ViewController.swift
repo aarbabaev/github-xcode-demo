63a4bfd598c4ab50d6a486df785faa84148181ee
Arman Arbabaev <aarbabaev@armans-macbook-pro.local>
2016-08-13 10:02:45 +0500

    ,m.,

--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -17,6 +17,11 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+      
+        
+        
+        
+        
         // Dispose of any resources that can be recreated.
     }
 
